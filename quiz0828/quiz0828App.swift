303f1c2f8babb7f09ac39d9bb837df5897028747
Kawarai-S <kayawakakururu@icloud.com>
2023-08-28 12:57:05 +0900

    Initial Commit

---
+++ quiz0828/quiz0828App.swift
@@ -0,0 +1,17 @@
+//
+//  quiz0828App.swift
+//  quiz0828
+//
+//  Created by 瓦井つばさ on 2023/08/28.
+//
+
+import SwiftUI
+
+@main
+struct quiz0828App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
